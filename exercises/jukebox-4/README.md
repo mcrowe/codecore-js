c46c3e15df684a79ff183c70b856044411fe252c
Mitch Crowe <crowe.mitch@gmail.com>
2015-02-01 09:28:30 -0800

    Clean up jukebox 4 exercise.

--- exercises/jukebox-4/README.md
+++ exercises/jukebox-4/README.md
@@ -1,7 +1,24 @@
-- 1) Add jQuery UI to the page
-- 2) Make the song notes hiddent by default
-- 3) Show song notes when you double click a song (see dblclick event).
-- 2) Filtering library
-- 3) Make library sortable
-- 4) Allow dropping songs from library to playlist.
-- Add a cool animation of your own.
+# Jukebox 4
+
+Our amazing designer has our Jukebox looking pretty good. Now it's time to add some animations to really spruce things up.
+
+## Part 1
+- Read through `index.html` and `app.js` and make sure you understand everything that's there.
+- When the delete icons for a song are clicked slide up that song over 0.5s. AFTER the song is finished sliding up, remove it from the list entirely.
+- Make the song notes hidden when the page initially loads. Then, when you double click a song, they should slide down over 0.3 second. (Hint: See the `dblclick` event type)
+- When the page loads, make the message fade in over 0.8s. Then, after 3s have passed, fade out the message over 0.8s.
+- Make the songs' delete icons have a default opacity of 0.3. When your mouse hovers over a song, animate its delete icon's opacity to 1 using CSS transitions. (Hint: The ":hover" CSS Selector may be helpful)
+- When a song is hovered, animate its background color to a lighter grey over 0.5s, using CSS selectors.
+
+## Part 2
+- Add a script tag to load jQuery UI javascript on the page, using Google's CDN (google: "jQuery UI google CDN").
+- Make the Library and Playlist sortable.
+- Allow dragging and dropping songs between the Library and the Playlist.
+
+## Part 3
+- When a song is played and then returned to the library, try double clicked it to show the notes. If you weren't careful, it won't work anymore! Use event delegation with the `on` method to fix this problem.
+- Filter the library, so that it includes only songs that match whatever is typed in the "filter" box. (Hint: Look up the `:contains` selector or the `filter` jQuery method)
+
+## Part 4
+- Make the "Play" button shake when it is clicked but there are no songs in the playlist. Use CSS keyframe animations.
+- Make the JukeBox logo do a dance while the Jukebox is playing, and stop when it's finished, using CSS keyframe animations. (Hint: Use your imagination).
--- exercises/jukebox-4/app.js
+++ exercises/jukebox-4/app.js
@@ -1,70 +1,83 @@
-// Add some songs to the library.
-var initializeLibrary = function() {
-  var library = [
-    {title: 'C Major Scale', notes: 'A B C D E F G' },
-    {title: 'Chromatic Scale', notes: 'A A# B C C# D D# E F F# G G#' },
-    {title: 'Random Song', notes: 'A B*2 C D A*4 D E*2 F A B A A*2' },
-    {title: 'Adup Licate', notes: 'A B*2 C D A*4 D E*2 F A B A A*2' },
-    {title: 'Yankee Doodle', notes: 'C F*4 C F*4 B C D A*2 B*2 A B*2 C' }
-  ];
+var LIBRARY = [
+  {title: 'C Major Scale', notes: 'A B C D E F G' },
+  {title: 'Chromatic Scale', notes: 'A A# B C C# D D# E F F# G G#' },
+  {title: 'Random Song', notes: 'A B*2 C D A*4 D E*2 F A B A A*2' },
+  {title: 'Adup Licate', notes: 'A B*2 C D A*4 D E*2 F A B A A*2' },
+  {title: 'Yankee Doodle', notes: 'C F*4 C F*4 B C D A*2 B*2 A B*2 C' },
+  {title: 'Descending Notes', notes: 'G F E D C B A G F E D C B A' }
+];
 
-  for(var i=0; i < library.length; i+=1) {
-    var song = library[i];
+var BPM = 600;
 
-    $('#library-list').append("<li>" +
+
+// Add a song with the given title and notes to the library.
+var addSongToLibrary = function(title, notes) {
+  $('#library-list').append("<li>" +
                                 "<i class='fa fa-bars'></i>" +
-                                "<span class='title'>" + song.title + "</span>" +
-                                "<div class='notes'>" + song.notes + "</div>" +
+                                "<i class='fa fa-trash'></i>" +
+                                "<span class='title'>" + title + "</span>" +
+                                "<div class='notes'>" + notes + "</div>" +
                               "</li>");
+};
+
+
+// Add all LIBRARY songs to the library.
+var initializeLibrary = function() {
+  for(var i=0; i < LIBRARY.length; i+=1) {
+    addSongToLibrary(LIBRARY[i].title, LIBRARY[i].notes);
   }
 };
 
 
-// Grab the top song in the queue, parse its notes and play them.
-// Then recurse until there are no more songs left in the queue.
-//
-var playNextSong = function() {
-  var songItem = $('#playlist-list li:first-child');
+// Play all songs in the playlist.
+var playAll = function() {
 
-  if (songItem.length == 0) {
-    $('.alert').fadeOut();
-    return;
-  }
+  // Grab the top song in the queue, parse its notes and play them.
+  // Then recurse until there are no more songs left in the queue.
+  //
+  var playNext = function() {
+    var songItem = $('#playlist-list li:first-child');
 
-  var title = songItem.find('.title').text();
-  var notes = songItem.find('.notes').text();
+    if (songItem.length == 0) {
+      // No more songs.
 
-  $('.alert').html("Now playing: <strong>" + title + "</strong>").show();
+      // Re-enable the play button.
+      $('#play-button').attr('disabled', false).text('Play All');
 
-  playSong( parseSong(notes), 600, function() {
-    songItem.remove();
-    $('#library-list').append(songItem);
-    playNextSong();
-  });
-};
+      // Fade out the message.
+      $('#message').fadeOut();
+      return;
+    }
+
+    var title = songItem.find('.title').text();
+    var notes = songItem.find('.notes').text();
+    var song = parseSong(notes);
+
+    $('#message').html("Now playing: <strong>" + title + "</strong>").show();
+
+    playSong(song, BPM, function() {
+      songItem.remove();
+      $('#library-list').append(songItem);
+      playNext();
+    });
+  };
+
+  // Disable the play button to start.
+  $('#play-button').attr('disabled', true).text('Playing');
+
+  playNext();
+}
 
 
 $(document).ready(function() {
 
+  // Initialize the library with some songs.
   initializeLibrary();
 
-  // Play playlist on "play".
-  $('#play').on('click', playNextSong);
+  // Play all songs in the playlist when the "play" button is clicked.
+  $('#play-button').on('click', playAll);
 
-  // Filter library results.
-  $('#filter-library').on('keyup', function() {
-    var query = $(this).val();
-    $('#library-list li').hide();
-    $('#library-list li:contains(' + query + ')').show();
-  });
 
-  // Sortable / Draggable lists.
-  $("#library-list").sortable({ connectWith: '#playlist-list'})
-  $("#playlist-list").sortable({ connectWith: '#library-list'});
-
-  // Double-click to view notes.
-  $('li').on('dblclick', function() {
-    $(this).find('.notes').slideToggle(300);
-  });
+  // Add Your Code Here.
 
 });
